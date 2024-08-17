f951554be55416a9ac161fe992ce8e2a9dd5a650
ViduraTharuka <111773568+ViduraTharuka@users.noreply.github.com>
2024-08-17 10:50:37 +0530

    Update App.jsx with basename for BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 
 import HomeScreen from '../src/screens/HomeScreen'
 import BookingScreen from '../src/screens/BookingScreen'
-import { Route, Routes } from 'react-router-dom'
+import { Route, Router, Routes } from 'react-router-dom'
 
 function App() {
   return (
-    <Routes>
-      <Route  path='/' element={<HomeScreen/>}/>
-      <Route path='/booking' element= {<BookingScreen/>}/>
-    </Routes>
+    <Router basename="/my-project">
+      <Routes>
+        <Route  path='/' element={<HomeScreen/>}/>
+        <Route path='/booking' element= {<BookingScreen/>}/>
+      </Routes>
+    </Router>
   )
 }
 
